d21f09d7e92275be83ded7d4c947f20b9211673f
Vitor Fernando da Silva <vitor_fernando@outlook.com.br>
2024-08-17 11:14:49 -0300

    feat: create simple app

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import subtract from "./calc/subtract";
+import sum from "./calc/sum";
+
+console.log("app running");
+
+const added = sum(1, 2);
+const subtracted = subtract(2, 1);
+
+console.log({ added, subtracted });
