8d53ad6ceeef6b37e691a584b85303c0296b2307
Vitor Fernando da Silva <vitor_fernando@outlook.com.br>
2024-08-17 12:50:30 -0300

    feat: broken test

--- src/tests/calc/sum.spec.ts
+++ src/tests/calc/sum.spec.ts
@@ -2,7 +2,7 @@ import sum from "../../calc/sum";
 
 describe("sum", () => {
   it("should sum correctly", () => {
-    const added = sum(2, 1);
+    const added = sum(1, 1);
     const expected = 3;
 
     expect(added).toBe(expected);
